99b174f47ba730258f4e986ad5f0308e3f8a53ad
206 <206@academicos.cesde.edu.co>
2022-12-05 12:16:32 -0500

    se crean clases, interfases y servicios

---
+++ src/main/java/com/example/viaticosonline/repositorios/RepositorioEmpleado.java
@@ -0,0 +1,8 @@
+package com.example.viaticosonline.repositorios;
+
+import com.example.viaticosonline.entidades.Empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositorioEmpleado extends JpaRepository<Empleado,Integer> {
+
+}
---
+++ src/main/java/com/example/viaticosonline/servicios/ServiciosBase.java
@@ -0,0 +1,12 @@
+package com.example.viaticosonline.servicios;
+
+import java.util.List;
+
+public interface ServiciosBase <E> {
+
+    public List<E> buscarTodos() throws Exception;
+    public  E buscarPorId(Integer id) throws  Exception;
+    public  E registrar(E entidad) throws  Exception;
+
+    public  E editar(Integer id, E entidad) throws Exception;
+}
